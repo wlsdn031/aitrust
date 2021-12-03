1f970a532ce87b383dd23f944200b2c88040a768
wlsdn031 <wlsdn0319@gmail.com>
2021-12-03 14:01:26 +0900

    Add files via upload

--- testingtrash.py
+++ testingtrash.py
@@ -15,6 +15,7 @@ havelist = []
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
+    time.sleep(0.2)
     for b in balances:
         if b['currency'] == ticker:
             if b['balance'] is not None:
@@ -26,6 +27,7 @@ def get_balance(ticker):
 def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+    time.sleep(0.2)
 
 
 def checking(ticker):
@@ -64,7 +66,7 @@ def targeting():
         
         if RSI < 25:
             if slow_d < slow_k < 25 :
-                buyinglist.append(ticker)
+                buyinglist.append(coin)
     print(buyinglist)
 
 print("autotrade start")
@@ -72,16 +74,34 @@ upbit = pyupbit.Upbit(access, secret)
 
 while True:
     try:
-        krw = get_balance("KRW")
         for coin in havelist:
-            checking(coin)
-            print(buyinglist)
+            df=pyupbit.get_ohlcv(coin, interval='minute5', count=15)
+            time.sleep(0.2)
+            df['up'] = np.where(df.diff(1)['close'] > 0, df.diff(1)['close'], 0)
+            df['down'] = np.where(df.diff(1)['close'] < 0, df.diff(1)['close']*(-1), 0)
+            df['ua'] = df['up'].rolling(window=14).mean()
+            df['da'] = df['down'].rolling(window=14).mean()
+            df['RSI'] = df['ua']*100 / (df['da'] + df['ua'])
+            df['fast_k']=100*((df['close']-df['low'].rolling(9).min()) / (df['high'].rolling(9).max()-df['low'].rolling(9).min()))
+            df['slow_k']=df['fast_k'].rolling(3).mean()
+            df['slow_d']=df['slow_k'].rolling(3).mean()
+            slow_d = df['slow_d'][-1]
+            slow_k = df['slow_k'][-1]
+
+            RSI = (df['ua'][-1] / (df['ua'][-1]+df['da'][-1])) * 100
             if RSI > 65:
                 if 75 < slow_k < slow_d: 
                     btc = get_balance(coin[4:])
-                    upbit.sell_market_order(goldname, btc*0.9995)
+                    upbit.sell_market_order(coin, btc*0.9995)
                     havelist.remove(coin)
                     print("sell", coin)
+                else:
+                    pass
+            else:
+                pass
+        krw = get_balance("KRW")
+        print(buyinglist)
+        
         if 8000 < krw:
             targeting()
             buyinglist=list(set(buyinglist)-set(havelist))
@@ -94,12 +114,16 @@ while True:
                 krwf = math.floor(krw*0.0001)
                 if len(buyinglist) > krwf :
                     new_buyinglist=buyinglist[0:krwf]
+                elif 1 <= len(buyinglist) and len(buyinglist) <= krwf  :
+                    new_buyinglist=buyinglist[0:len(buyinglist)]
+            print(new_buyinglist)
             for coin in new_buyinglist:
                 upbit.buy_market_order(coin, 9995)
                 havelist.append(coin)
                 print("buy" + coin)
         time.sleep(1)
         print("cycle")
+        buyinglist=[]
     except Exception as e:
         print(e)
         time.sleep(1)
