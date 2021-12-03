e73d1cb668836aab7cca25f965f6a9d0db2bce59
wlsdn031 <wlsdn0319@gmail.com>
2021-12-03 21:54:42 +0900

    Add files via upload

--- testingtrash.py
+++ testingtrash.py
@@ -9,9 +9,8 @@ import datetime
 
 access = "0hNyHckgUEQ0GpFb97xmHOtLGKx8AevNu7pzz2Vo"
 secret = "NtOH4y4d3G2I4gG13G1KVhAYhPck2xWMxLd6xYvd"
-
+havelist=[]
 buyinglist = []
-havelist = []
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
@@ -28,7 +27,6 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
     time.sleep(0.2)
-
 
 def checking(ticker):
     df=pyupbit.get_ohlcv(ticker, interval='minute5', count=15)
@@ -50,80 +48,113 @@ def targeting():
     tickers = pyupbit.get_tickers(fiat="KRW")
     time.sleep(0.2)
     for coin in tickers:
-        df=pyupbit.get_ohlcv(coin, interval='minute5', count=15)
+        df=pyupbit.get_ohlcv(coin, interval='minute1', count=80)
         time.sleep(0.2)
-        df['up'] = np.where(df.diff(1)['close'] > 0, df.diff(1)['close'], 0)
-        df['down'] = np.where(df.diff(1)['close'] < 0, df.diff(1)['close']*(-1), 0)
-        df['ua'] = df['up'].rolling(window=14).mean()
-        df['da'] = df['down'].rolling(window=14).mean()
+        df['up'] = np.where(df.diff(5)['close'] > 0, df.diff(5)['close'], 0)
+        df['down'] = np.where(df.diff(5)['close'] < 0, df.diff(5)['close']*(-1), 0)
+        df['ua'] = df['up'].rolling(window=70).mean()
+        df['da'] = df['down'].rolling(window=70).mean()
         df['RSI'] = df['ua']*100 / (df['da'] + df['ua'])
-        df['fast_k']=100*((df['close']-df['low'].rolling(3).min()) / (df['high'].rolling(3).max()-df['low'].rolling(3).min()))
-        df['slow_k']=df['fast_k'].rolling(3).mean()
-        df['slow_d']=df['slow_k'].rolling(3).mean()
-        slow_d = df['slow_d'][-1]
-        slow_k = df['slow_k'][-1]
-        RSI = (0.35*df['RSI'][-1]) + (0.65*df['RSI'][-2])
-        
-        if RSI < 25:
-            if slow_d < slow_k < 25 :
+        df['fast_k']=100*((df['close']-df['low'].rolling(45).min()) / (df['high'].rolling(45).max()-df['low'].rolling(45).min()))
+        df['slow_k']=df['fast_k'].rolling(15).mean()
+        df['slow_d']=df['slow_k'].rolling(15).mean()
+        slow_d = df['slow_d'][-2]
+        slow_k = df['slow_k'][-2]
+
+        RSI = df['ua'][-2]*100 / (df['da'][-2] + df['ua'][-2])
+        if RSI < 100:
+            if slow_d < slow_k < 100 :
                 buyinglist.append(coin)
     print(buyinglist)
+    
+
+def trading():
+    buyinglist=[]
+    for coin in havelist:
+        df=pyupbit.get_ohlcv(coin, interval='minute1', count=80)
+        time.sleep(0.2)
+        df['up'] = np.where(df.diff(5)['close'] > 0, df.diff(5)['close'], 0)
+        df['down'] = np.where(df.diff(5)['close'] < 0, df.diff(5)['close']*(-1), 0)
+        df['ua'] = df['up'].rolling(window=70).mean()
+        df['da'] = df['down'].rolling(window=70).mean()
+        df['RSI'] = df['ua']*100 / (df['da'] + df['ua'])
+        df['fast_k']=100*((df['close']-df['low'].rolling(45).min()) / (df['high'].rolling(45).max()-df['low'].rolling(45).min()))
+        df['slow_k']=df['fast_k'].rolling(15).mean()
+        df['slow_d']=df['slow_k'].rolling(15).mean()
+        slow_d = df['slow_d'][-2]
+        slow_k = df['slow_k'][-2]
+
+        RSI = df['ua'][-2]*100 / (df['da'][-2] + df['ua'][-2])
+        if RSI > 65:
+            if 75 < slow_k < slow_d: 
+                btc = get_balance(coin[4:])
+                upbit.sell_market_order(coin, btc*0.9995)
+                havelist.remove(coin)
+                print("sell", coin)
+            else:
+                pass
+        else:
+            pass
+    krw = get_balance("KRW")
+    print(buyinglist)
+    
+    if len(havelist) > 0:
+        pass
+    elif 8000 < krw:
+        buyinglist=[]
+
+        tickers = pyupbit.get_tickers(fiat="KRW")
+        time.sleep(0.2)
+        for coin in tickers:
+            df=pyupbit.get_ohlcv(coin, interval='minute1', count=80)
+            time.sleep(0.2)
+            df['up'] = np.where(df.diff(5)['close'] > 0, df.diff(5)['close'], 0)
+            df['down'] = np.where(df.diff(5)['close'] < 0, df.diff(5)['close']*(-1), 0)
+            df['ua'] = df['up'].rolling(window=70).mean()
+            df['da'] = df['down'].rolling(window=70).mean()
+            df['RSI'] = df['ua']*100 / (df['da'] + df['ua'])
+            df['fast_k']=100*((df['close']-df['low'].rolling(45).min()) / (df['high'].rolling(45).max()-df['low'].rolling(45).min()))
+            df['slow_k']=df['fast_k'].rolling(15).mean()
+            df['slow_d']=df['slow_k'].rolling(15).mean()
+            slow_d = df['slow_d'][-2]
+            slow_k = df['slow_k'][-2]
+
+            RSI = df['ua'][-2]*100 / (df['da'][-2] + df['ua'][-2])
+            if RSI < 25:
+                if slow_d < slow_k < 25 :
+                    buyinglist.append(coin)
+        
+        print(buyinglist)
+        buyinglist=list(set(buyinglist)-set(havelist))
+        new_buyinglist=[]
+        if len(buyinglist) == 0:
+            print(len(buyinglist))
+            pass
+        elif 0<(krw*0.0001)<2:
+            new_buyinglist.append(buyinglist[0])
+        else:
+            krwf = math.floor(krw*0.0001)
+            if len(buyinglist) > krwf :
+                new_buyinglist=buyinglist[0:krwf]
+                print(new_buyinglist)
+            elif (1 <= len(buyinglist)) and (len(buyinglist) <= krwf)  :
+                new_buyinglist=buyinglist[0:len(buyinglist)]
+                print(new_buyinglist)        
+        print(new_buyinglist)
+        for coin in new_buyinglist:
+            upbit.buy_market_order(coin, 9995)
+            havelist.append(coin)
+            print("buy" + coin)
+    time.sleep(1)
+    print("cycle")
+    buyinglist=[]
 
 print("autotrade start")
 upbit = pyupbit.Upbit(access, secret)
 
-while True:
-    try:
-        for coin in havelist:
-            df=pyupbit.get_ohlcv(coin, interval='minute5', count=15)
-            time.sleep(0.2)
-            df['up'] = np.where(df.diff(1)['close'] > 0, df.diff(1)['close'], 0)
-            df['down'] = np.where(df.diff(1)['close'] < 0, df.diff(1)['close']*(-1), 0)
-            df['ua'] = df['up'].rolling(window=14).mean()
-            df['da'] = df['down'].rolling(window=14).mean()
-            df['RSI'] = df['ua']*100 / (df['da'] + df['ua'])
-            df['fast_k']=100*((df['close']-df['low'].rolling(9).min()) / (df['high'].rolling(9).max()-df['low'].rolling(9).min()))
-            df['slow_k']=df['fast_k'].rolling(3).mean()
-            df['slow_d']=df['slow_k'].rolling(3).mean()
-            slow_d = df['slow_d'][-1]
-            slow_k = df['slow_k'][-1]
+schedule.every().minute.at(":00").do(trading)
 
-            RSI = (0.35*df['RSI'][-1]) + (0.65*df['RSI'][-2])
-            if RSI > 65:
-                if 75 < slow_k < slow_d: 
-                    btc = get_balance(coin[4:])
-                    upbit.sell_market_order(coin, btc*0.9995)
-                    havelist.remove(coin)
-                    print("sell", coin)
-                else:
-                    pass
-            else:
-                pass
-        krw = get_balance("KRW")
-        print(buyinglist)
-        
-        if 8000 < krw:
-            targeting()
-            buyinglist=list(set(buyinglist)-set(havelist))
-            new_buyinglist=[]
-            if len(buyinglist) == 0:
-                pass
-            elif 0<(krw*0.0001)<2:
-                new_buyinglist.append(buyinglist[0])
-            else:
-                krwf = math.floor(krw*0.0001)
-                if len(buyinglist) > krwf :
-                    new_buyinglist=buyinglist[0:krwf]
-                elif 1 <= len(buyinglist) and len(buyinglist) <= krwf  :
-                    new_buyinglist=buyinglist[0:len(buyinglist)]
-            print(new_buyinglist)
-            for coin in new_buyinglist:
-                upbit.buy_market_order(coin, 9995)
-                havelist.append(coin)
-                print("buy" + coin)
-        time.sleep(1)
-        print("cycle")
-        buyinglist=[]
-    except Exception as e:
-        print(e)
-        time.sleep(1)
+while True:
+    
+    schedule.run_pending()
+    time.sleep(1)
